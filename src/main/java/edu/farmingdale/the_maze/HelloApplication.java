d9c224ff1d48920974f041648e12f96f39a3c061
JPR <jonatanp613@gmail.com>
2025-02-15 21:09:38 -0500

    commit

--- src/main/java/edu/farmingdale/the_maze/HelloApplication.java
+++ src/main/java/edu/farmingdale/the_maze/HelloApplication.java
@@ -12,12 +12,12 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 884, 652);
-
         stage.setResizable(false);
         stage.setTitle("Robot MiniGame");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/edu/farmingdale/the_maze/HelloController.java
+++ src/main/java/edu/farmingdale/the_maze/HelloController.java
@@ -1,14 +1,10 @@
 package edu.farmingdale.the_maze;
 
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
+import javafx.scene.image.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-
+import javafx.scene.paint.Color;
 
 public class HelloController {
 
@@ -22,11 +18,14 @@ public class HelloController {
     Image robotPlayer = new Image(String.valueOf(getClass().getResource("/robot.png")));
     Image mazePicture = new Image(String.valueOf(getClass().getResource("/maze.png")));
 
+    PixelReader readerX = mazePicture.getPixelReader();
+    private final Color WALL_COLOR = readerX.getColor(5, 5);
+    private final Color PATH_COLOR = Color.WHITE;
+
     @FXML
     public void initialize() {
         maze.setImage(mazePicture);
         robot.setImage(robotPlayer);
-
 
         robot.setFocusTraversable(true);
         robot.requestFocus();
@@ -37,22 +36,51 @@ public class HelloController {
         double currentX = robot.getLayoutX();
         double currentY = robot.getLayoutY();
 
-
         switch (event.getCode()) {
             case UP:
-                robot.setLayoutY(currentY - 10);
+                if (isValidMove(currentX, currentY - 10)) {
+                    robot.setLayoutY(currentY - 10);
+                }
                 break;
             case DOWN:
-                robot.setLayoutY(currentY + 10);
+                if (isValidMove(currentX, currentY + 10)) {
+                    robot.setLayoutY(currentY + 10);
+                }
                 break;
             case LEFT:
-                robot.setLayoutX(currentX - 10);
+                if (isValidMove(currentX - 10, currentY)) {
+                    robot.setLayoutX(currentX - 10);
+                }
                 break;
             case RIGHT:
-                robot.setLayoutX(currentX + 10);
+                if (isValidMove(currentX + 10, currentY)) {
+                    robot.setLayoutX(currentX + 10);
+                }
                 break;
             default:
                 break;
         }
+    }
+
+
+    private boolean isValidMove(double x, double y) {
+
+        PixelReader reader = mazePicture.getPixelReader();
+
+        if (reader != null) {
+
+            int pixelX = (int) (x + robot.getFitWidth() / 2);
+            int pixelY = (int) (y + robot.getFitHeight() / 2);
+
+
+            if (pixelX >= 0 && pixelY >= 0 && pixelX < maze.getImage().getWidth() && pixelY < maze.getImage().getHeight()) {
+
+                Color color = reader.getColor(pixelX, pixelY);
+
+
+                return !color.equals(WALL_COLOR);
+            }
+        }
+        return false;
     }
 }
